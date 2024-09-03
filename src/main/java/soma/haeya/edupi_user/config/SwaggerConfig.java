9754b66824fb77eec858301348bee675f850d470
UJeans <jean207@naver.com>
2024-09-03 16:27:00 +0900

    [#27]fix: 중복 로직 제거, 함수 분리

--- src/main/java/soma/haeya/edupi_user/config/SwaggerConfig.java
+++ src/main/java/soma/haeya/edupi_user/config/SwaggerConfig.java
@@ -12,28 +12,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "JWT";
+    private static final String SCHEME_TYPE = "bearer";
+
     @Bean
     public OpenAPI openAPI() {
-        String jwt = "JWT";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
-        Components components = new Components()
-            .addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-            );
         return new OpenAPI()
-            .components(new Components())
             .info(apiInfo())
-            .addSecurityItem(securityRequirement)
-            .components(components);
+            .addSecurityItem(createSecurityRequirement())
+            .components(createComponents());
+    }
+
+    private SecurityRequirement createSecurityRequirement() {
+        return new SecurityRequirement().addList(SECURITY_SCHEME_NAME);
     }
 
     private Info apiInfo() {
         return new Info()
-            .title("API") // API의 제목
-            .description("Let's practice Swagger UI") // API에 대한 설명
-            .version("1.0.0"); // API의 버전
+            .title("Edupi User API")
+            .version("1.0.0");
+    }
+
+    private Components createComponents() {
+        return new Components()
+            .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                new SecurityScheme()
+                    .name(SECURITY_SCHEME_NAME)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme(SCHEME_TYPE)    // JWT 토큰이 필요한 API 테스트를 위해 명시
+                    .bearerFormat(SECURITY_SCHEME_NAME)
+            );
     }
 }
