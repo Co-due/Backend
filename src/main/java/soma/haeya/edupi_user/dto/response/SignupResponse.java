2228cd8cf78d59cce96b8a95c85dae61d081f675
UJeans <jean207@naver.com>
2024-09-05 19:04:10 +0900

    [#26]fix: 기본 생성자 추가

--- src/main/java/soma/haeya/edupi_user/dto/response/SignupResponse.java
+++ src/main/java/soma/haeya/edupi_user/dto/response/SignupResponse.java
@@ -1,14 +1,13 @@
 package soma.haeya.edupi_user.dto.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SignupResponse {
 
     String message;
-
-    public SignupResponse() {
-    }
 
     public SignupResponse(String message) {
         this.message = message;
