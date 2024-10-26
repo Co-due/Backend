496abe2d5a956cf8a15a764af5a68fd1267efc63
UJeans <jean207@naver.com>
2024-10-26 15:42:04 +0900

    [#48]feat: 주석 삭제, cors 설정 삭제

--- src/main/java/soma/edupiuser/oauth/config/SecurityConfig.java
+++ src/main/java/soma/edupiuser/oauth/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package soma.edupiuser.oauth.config;
 
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import soma.edupiuser.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
 import soma.edupiuser.oauth.handler.OAuth2AuthenticationFailureHandler;
 import soma.edupiuser.oauth.handler.OAuth2AuthenticationSuccessHandler;
-import soma.edupiuser.oauth.jwt.JwtAuthorizationFilter;
 import soma.edupiuser.oauth.service.CustomOAuth2UserService;
 
 @RequiredArgsConstructor
@@ -29,7 +25,6 @@ public class SecurityConfig {
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
-    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -43,26 +38,18 @@ public class SecurityConfig {
                 HeadersConfigurer.FrameOptionsConfig::disable)) // For H2 DB
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers(antMatcher("/edupi-user/oauth2/**")).permitAll()
-                .requestMatchers(antMatcher("/edupi-user/v1/account/**")).permitAll()
-                .anyRequest().authenticated()
-            )
             .sessionManagement(sessions -> sessions
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
             .oauth2Login(configure ->
                     configure
                         .authorizationEndpoint(config -> {
-//                        config.baseUri("/edupi_user/oauth2/authorization");
+//                        config.baseUri("/oauth2/authorization");
                             config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository);
                         })
-                        .userInfoEndpoint(config ->
-                            config.userService(customOAuth2UserService))
+                        .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
                         .successHandler(oAuth2AuthenticationSuccessHandler) // 인증 성공 시 처리
                         .failureHandler(oAuth2AuthenticationFailureHandler) //  인증 실패 시 처리
             );
-        //http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
