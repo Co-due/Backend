b1646576247a7870ababc34e0b13361ac9e712c0
UJeans <jean207@naver.com>
2024-10-28 00:43:57 +0900

    [#48]feat: 인증 요구 설정 추가

--- src/main/java/soma/edupiuser/oauth/config/SecurityConfig.java
+++ src/main/java/soma/edupiuser/oauth/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package soma.edupiuser.oauth.config;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
