c7bf91d8f40ea7d08aa31d345129590209ae8824
UJeans <jean207@naver.com>
2024-10-29 15:20:02 +0900

    [#52]fix: 안 쓰는 변수 삭제

--- src/main/java/soma/edupiuser/oauth/models/GoogleOAuth2UserInfo.java
+++ src/main/java/soma/edupiuser/oauth/models/GoogleOAuth2UserInfo.java
@@ -6,24 +6,14 @@ public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
 
     private final Map<String, Object> attributes;
     private final String accessToken;
-    private final String id;
     private final String email;
     private final String name;
-    private final String firstName;
-    private final String lastName;
-    private final String nickName;
-    private final String profileImageUrl;
 
     public GoogleOAuth2UserInfo(String accessToken, Map<String, Object> attributes) {
         this.accessToken = accessToken;
         this.attributes = attributes;
-        this.id = (String) attributes.get("sub");
         this.email = (String) attributes.get("email");
         this.name = (String) attributes.get("name");
-        this.firstName = (String) attributes.get("given_name");
-        this.lastName = (String) attributes.get("family_name");
-        this.nickName = null;
-        this.profileImageUrl = (String) attributes.get("picture");
     }
 
     @Override
@@ -42,11 +32,6 @@ public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
     }
 
     @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
     public String getEmail() {
         return email;
     }
@@ -54,25 +39,5 @@ public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
     @Override
     public String getName() {
         return name;
-    }
-
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
-
-    @Override
-    public String getNickname() {
-        return nickName;
-    }
-
-    @Override
-    public String getProfileImageUrl() {
-        return profileImageUrl;
     }
 }
