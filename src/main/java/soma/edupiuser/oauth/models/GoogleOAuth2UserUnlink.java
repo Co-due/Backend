f8b96005dc60044764b90f281bf5dd459a32c254
UJeans <jean207@naver.com>
2024-11-04 18:13:41 +0900

    [#48]feat: restTemplate -> restClient 사용

--- src/main/java/soma/edupiuser/oauth/models/GoogleOAuth2UserUnlink.java
+++ src/main/java/soma/edupiuser/oauth/models/GoogleOAuth2UserUnlink.java
@@ -3,21 +3,16 @@ package soma.edupiuser.oauth.models;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import soma.edupiuser.web.client.GoogleOAuth2ApiClient;
 
 @RequiredArgsConstructor
 @Component
 public class GoogleOAuth2UserUnlink implements OAuth2UserUnlink {
 
-    private static final String URL = "https://oauth2.googleapis.com/revoke";
-    private final RestTemplate restTemplate;
+    private final GoogleOAuth2ApiClient googleOAuth2ApiClient;
 
     @Override
     public void unlink(String accessToken) {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("token", accessToken);
-        restTemplate.postForObject(URL, params, String.class);
+        googleOAuth2ApiClient.unlink(accessToken);
     }
 }
