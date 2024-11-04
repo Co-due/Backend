9c87c41497ee51581882f158297c349a5d22bfa3
UJeans <jean207@naver.com>
2024-11-04 16:55:41 +0900

    [#52]fix: '+' 문자연산 -> UriComponentsBuilder 사용

--- src/main/java/soma/edupiuser/oauth/models/NaverOAuth2UserUnlink.java
+++ src/main/java/soma/edupiuser/oauth/models/NaverOAuth2UserUnlink.java
@@ -1,11 +1,10 @@
 package soma.edupiuser.oauth.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Component
@@ -22,29 +21,16 @@ public class NaverOAuth2UserUnlink implements OAuth2UserUnlink {
     @Override
     public void unlink(String accessToken) {
 
-        String url = URL +
-            "?service_provider=NAVER" +
-            "&grant_type=delete" +
-            "&client_id=" +
-            clientId +
-            "&client_secret=" +
-            clientSecret +
-            "&access_token=" +
-            accessToken;
+        String url = UriComponentsBuilder.fromHttpUrl(URL)
+            .queryParam("service_provider", "NAVER")
+            .queryParam("grant_type", "delete")
+            .queryParam("client_id", clientId)
+            .queryParam("client_secret", clientSecret)
+            .queryParam("access_token", accessToken)
+            .toUriString();
 
-        UnlinkResponse response = restTemplate.getForObject(url, UnlinkResponse.class);
+        restTemplate.getForObject(url, String.class);
 
-        if (response != null && !"success".equalsIgnoreCase(response.getResult())) {
-            throw new RuntimeException("Failed to Naver Unlink");
-        }
     }
 
-    @Getter
-    @RequiredArgsConstructor
-    public static class UnlinkResponse {
-
-        @JsonProperty("access_token")
-        private final String accessToken;
-        private final String result;
-    }
 }
