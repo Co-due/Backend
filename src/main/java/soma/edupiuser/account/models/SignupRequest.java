e03af4e42de37a71a7280b6e9f8bf5763439ac24
UJeans <jean207@naver.com>
2024-11-08 17:54:05 +0900

    [#77]fix: 회원가입 폼 통일

--- src/main/java/soma/edupiuser/account/models/SignupRequest.java
+++ src/main/java/soma/edupiuser/account/models/SignupRequest.java
@@ -14,16 +14,13 @@ import lombok.ToString;
 @NoArgsConstructor
 public class SignupRequest {
 
-    @NotBlank(message = "이메일은 공백이 아니어야 합니다.")
     @Email(message = "잘못된 이메일 형식입니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 공백이 아니어야 합니다.")
-    @Size(min = 8, max = 20, message = "비밀번호는 8에서 20자 이내여야 합니다.")
-    @Pattern(regexp = "^(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).*$", message = "비밀번호는 최소 1개의 특수기호를 포함해야 합니다.")
+    @Size(min = 8, max = 32, message = "비밀번호는 8에서 20자 이내여야 합니다.")
     private String password;
 
-    @NotBlank(message = "이름은 공백이 아니어야 합니다.")
     private String name;
 
     @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$",
