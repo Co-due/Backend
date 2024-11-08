88d67c9f2db36d953fc3746532c17cdae13265ac
UJeans <jean207@naver.com>
2024-11-08 19:06:16 +0900

    [#77]chore: 두가지 타입 지원

--- src/main/java/soma/edupiuser/account/models/SignupRequest.java
+++ src/main/java/soma/edupiuser/account/models/SignupRequest.java
@@ -19,6 +19,10 @@ public class SignupRequest {
 
     @NotBlank(message = "비밀번호는 공백이 아니어야 합니다.")
     @Size(min = 8, max = 20, message = "비밀번호는 8에서 20자 이내여야 합니다.")
+    @Pattern(
+        regexp = "^(?:(?=.*[a-zA-Z])(?=.*\\d)|(?=.*[a-zA-Z])(?=.*[\\W_])|(?=.*\\d)(?=.*[\\W_])).*$",
+        message = "비밀번호는 영문자와 숫자, 특수문자 중 적어도 두 가지를 포함해야 합니다."
+    )
     private String password;
 
     private String name;
