da398a92fcfda6523ecbc8640804330a13511e08
GS_song98 <20003204@sju.ac.kr>
2024-11-15 13:49:19 +0900

    [#80]refactor: token이 없는 경우 400이 아닌 202 상태코드를 내려준다.

--- src/main/java/soma/edupiuser/web/exception/ErrorEnum.java
+++ src/main/java/soma/edupiuser/web/exception/ErrorEnum.java
@@ -5,8 +5,11 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorEnum {
-    // 400
-    TOKEN_EXPIRE(HttpStatus.BAD_REQUEST, "AC-400001", "The token has expired."),
+    // 200
+    TOKEN_EXPIRE(HttpStatus.ACCEPTED, "AC-202001", "The token has expired."),
+    TOKEN_NOT_FOUND(HttpStatus.ACCEPTED, "AC-202002", "Token not found."),
+
+    // 400,
     INVALID_ACCOUNT(HttpStatus.BAD_REQUEST, "AC-400002", "ID or password is invalid."),
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "AC-400003", "It's a duplicate email."),
     OAUTH2_EXCEPTION(HttpStatus.BAD_REQUEST, "AC-400004", "Social login failed."),
@@ -15,10 +18,9 @@ public enum ErrorEnum {
         "Unable to get resources due to network issues. check the server is started."),
     RESPONSE_FORMAT_ERROR(HttpStatus.BAD_REQUEST, "AC-400006",
         "Invalid error response format."),
-    TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "AC-400007", "Token not found."),
     NOT_ACTIVATED_EXCEPTION(HttpStatus.BAD_REQUEST, "AC-400008", "Not Authenticated email user."),
 
-
+    // 500
     META_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "AC-500001", "Meta server Exception."),
     NOT_MATCH_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "AC-500002", "No matching error found.");
 
